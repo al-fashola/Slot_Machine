93f2bb284d192820608e8cfb5813c03ba0c4dbdb
a-fashola01 <128617918+a-fashola01@users.noreply.github.com>
2024-10-31 11:54:13 -0600

    game mode logic + win/lose if statement methodology

--- Slot_Machine/Program.cs
+++ Slot_Machine/Program.cs
@@ -7,18 +7,19 @@ class Program
         const int MATRIX_A = 3;
         const int NUMBER_OF_GAMES_OPTIONS = 4;
         const int MAX_SLOT_MACHINE_INT = 40;
+        
         const int CENTER_LINE_MODE = 1;
         const int HORIZONTAL_LINE_MODE = 2;
         const int VERTICAL_LINE_MODE = 3;
         const int ALL_DIAGONOL_LINE_MODE = 4;
-
+        const double CENTER_LINE_PAYOUT = 2.0 ;
+        const double HORIZONTAL_LINE_PAYOUT = 0.33 ;
+        const double VERTICAL_LINE_PAYOUT = 0.25;
+        const double ALL_DIAGONOL_LINE_PAYOUT = 0.75;
         
-        
-        
-
+        //Wager and Game mode variables 
         double wager = 0.0;
         int gameMode = 0;
-        
         
         Console.WriteLine("Let's play a game! Ths name of the game is a slot machine");
         Console.WriteLine("You will have the following options: \n" +
@@ -38,7 +39,6 @@ class Program
             gameMode = Convert.ToInt32(Console.ReadLine());
         }
         
-        
         // assign the size of array
         int[,] grid = new int[MATRIX_A,MATRIX_A];
         
@@ -53,46 +53,58 @@ class Program
                 grid[i,j] = rand.Next(0, MAX_SLOT_MACHINE_INT);
             }
         }
-        
-        
         /*
         Console.WriteLine(grid.Length);
         Console.WriteLine(gameMode);
         Console.WriteLine(wager);
         */
         
-        //Console.WriteLine(string.Join("", grid));  // how tp print a whole array?
-        
-        Console.WriteLine($"|{grid[0,0]}|{grid[0,1]}|{grid[0,2]}| \n" +
-                          $"|{grid[1,0]}|{grid[1,1]}|{grid[1,2]}| \n" +
-                          $"|{grid[2,0]}|{grid[2,1]}|{grid[2,2]}| \n");
-        
+        for (int x = 0; x < MATRIX_A; x++)
+        {
+            for (int y = 0; y < MATRIX_A; y++)
+            {
+                Console.Write($"|{grid[x, y]}|");
+            }
+            Console.WriteLine("");
+        }
         double payoutRate = 0.0;
         double totalPayout = 0.0;
         char gameWin = 'N';
 
-        // Logical breakdown to validate match in values accorss diagnals, horizontals, verticals
+        // Logical breakdown to validate match in values across diagonals, horizontals, verticals
         // This could be done as a method which checks for all the true cases of matches
-        if (gameMode == CENTER_LINE_MODE)
+        if (gameMode == CENTER_LINE_MODE && 
+            grid[1, 0] == grid[1, 1] && grid[1, 0] == grid[1, 2])
         {
-            
+                payoutRate = CENTER_LINE_PAYOUT;
+                gameWin = 'Y';
         }
-
         if (gameMode == HORIZONTAL_LINE_MODE)
         {
-            
+            payoutRate = HORIZONTAL_LINE_PAYOUT;
+            gameWin = 'Y';
         }
         if (gameMode == VERTICAL_LINE_MODE)
         {
             
+            payoutRate = VERTICAL_LINE_PAYOUT;
+            gameWin = 'Y';
         }
-        if (gameMode == ALL_DIAGONOL_LINE_MODE)
+        if (gameMode == ALL_DIAGONOL_LINE_MODE && 
+            ((grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2]) 
+             || (grid[2, 0] == grid[1, 1] && grid[1, 0] == grid[0, 2])))
         {
-            
+            payoutRate = ALL_DIAGONOL_LINE_PAYOUT;
+            gameWin = 'Y';
         }
+        if (gameWin == 'N')
+        {
+            Console.WriteLine("You Lose!");
+        }
+            
         
         //game win variable == Y and rate % variable value, ask to play again?
-        
+        totalPayout =  payoutRate * wager;
         //print message and calculate payout if won
         
         
